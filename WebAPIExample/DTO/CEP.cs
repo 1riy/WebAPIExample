a845bfebff13c480fcc4338ec32ed948a2cb4900
1riy <110687563+1riy@users.noreply.github.com>
2022-08-28 19:25:26 -0300

    Delete WebAPIExample directory

--- WebAPIExample/DTO/CEP.cs
+++
@@ -1,9 +0,0 @@
-public class CEP
-{
-    public string Logradouro { get; set; }
-    public string Complemento { get; set; }
-    public string Cep { get; set; }
-    public string Cidade { get; set; }
-    public string Bairro { get; set; }
-    public string Uf { get; set; }
-}
